030a6083ebbec6fea932f660d4e58a662465a01b
Andrey Levkov <levkov0406@mail.ru>
2023-02-20 22:41:49 +0300

    Revealed reduction of idf, tf. Add ratings to docs

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <execution>
 #include <cmath>
+#include <numeric>
 
 using namespace std;
 
@@ -26,6 +27,7 @@ int ReadLineWithNumber() {
 struct Document {
     int id;
     double relevance;
+    int rating;
 };
 
 struct Query {
@@ -38,7 +40,21 @@ class SearchServer {
     private:
         map<string, map<int, double>> word_in_document_freqs_;
         set<string> stop_words_;
+        map<int, int> doc_rating_;
         int document_count_ = 0;
+        
+        static int ComputeAverageRating(const vector<int>& ratings) {
+            return accumulate(ratings.begin(), ratings.end(), 0) / ratings.size();
+        }
+
+        vector<int> SplitIntoNumbers(const string& text) const {
+            vector<string> words = SplitIntoWords(text);
+            vector<int> rating;
+            for (int i=1;i<=stoi(words[0]);++i) {
+                rating.push_back(stoi(words[i]));
+            }
+            return rating;
+        }
 
         vector<string> SplitIntoWords(const string& text) const {
             vector<string> words;
@@ -76,12 +92,11 @@ class SearchServer {
             const Query query_words = ParseQuery(query);
             
             map<int, double> documents_relevance;
-            double idf, tf;
             for (const string& word : query_words.plus_words) {
                 if (word_in_document_freqs_.count(word) != 0) {
-                    idf = log(document_count_ / (word_in_document_freqs_.at(word).size() * 1.0));
-                    for (const auto& [document_id, tf]: word_in_document_freqs_.at(word)) {
-                        documents_relevance[document_id] += tf * idf;
+                    const double inverse_document_freq = log(document_count_ / (word_in_document_freqs_.at(word).size() * 1.0));
+                    for (const auto& [document_id, term_freq]: word_in_document_freqs_.at(word)) {
+                        documents_relevance[document_id] += term_freq * inverse_document_freq;
                     }
                 }
             }
@@ -96,7 +111,7 @@ class SearchServer {
 
             vector<Document> matched_documents;
                 for (auto& [document_id, relevance]: documents_relevance) {
-                    matched_documents.push_back({document_id, relevance});
+                    matched_documents.push_back({document_id, relevance, doc_rating_.at(document_id)});
             }
 
             return matched_documents;
@@ -125,14 +140,12 @@ class SearchServer {
         }
 
         void AddDocument(int document_id, const string& document) {
-            int word_count;
+            vector<int> doc_ratings = SplitIntoNumbers(ReadLine());
+            doc_rating_[document_id] = ComputeAverageRating(doc_ratings);
             vector<string> document_words = SplitIntoWordsNoStop(document);
-            set<string> words_set = {document_words.begin(), document_words.end()};
-            for (const string& word : words_set) {
-                word_count = count_if(document_words.begin(), 
-                                document_words.end(), 
-                                [&word](string& word_to_check) { return word_to_check == word; });
-                word_in_document_freqs_[word][document_id] = word_count / (document_words.size() * 1.0);
+            const double inv_words_count = 1.0 / document_words.size();
+            for (const string& word : document_words) {
+                word_in_document_freqs_[word][document_id] += inv_words_count;
                 }
             ++document_count_;
         }
@@ -175,8 +188,10 @@ int main() {
     const SearchServer search_server = CreateSearchServer();
 
     const string query = ReadLine();
-    for (auto [document_id, relevance] : search_server.FindTopDocuments(query)) {
-        cout << "{ document_id = "s << document_id << ", relevance = "s << relevance << " }"s
+    for (auto [document_id, relevance, rating] : search_server.FindTopDocuments(query)) {
+        cout << "{ document_id = "s << document_id << 
+                ", relevance = "s << relevance << 
+                ", rating = "s << rating << " }"s
              << endl;
     }
 
